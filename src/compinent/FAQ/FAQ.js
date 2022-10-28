32d1e11c901379a6ebf6a680b0b77f0a9e71f69a
abdulhamid331404 <freelancerhamid1@gmail.com>
2022-10-28 21:36:22 +0600

    added FAQ

--- src/compinent/FAQ/FAQ.js
+++ src/compinent/FAQ/FAQ.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const FAQ = () => {
     return (
-        <div>
-            <h1>This is FAQ</h1>
+        <div className='p-5' >
+            <h2>If you have any Question Pleace search google. <br />
+                 When We are Developing this site. <br />Then We Send Mail For You.  </h2>
+            <h3>Thank You!!</h3>
         </div>
     );
 };
--- src/compinent/Home/Home.js
+++ src/compinent/Home/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import logo from '../../images/home-logo.png'
 
 const Home = () => {
     return (
         <div>
-            <h1>This is HOme</h1>
+            
+                <h1 className='text-success'>Welcome to....</h1>
+                
+            <div>
+                 <img src={logo} className="w-100" alt="" />
+            </div>
         </div>
     );
 };
